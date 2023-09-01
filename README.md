729727a83b1db69ae8fab9827ca00c42c39ce975
Pete <MechEngineerPete@gmail.com>
2023-09-01 12:38:15 +0700

    General layout

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# flutter-calculator
-Copy of the Android calculator as a learning project to help me learn Flutter.
+# Flutter Calculator
+
+A calculator based on the Andriod calculator. 
+
+This project is for me to learn Flutter better and have a bit of fun trying to make the calculator look a bit different, hopefully better.
+
+## The Plan:
+
+This is a screen shot of the  Android calculator from my own phone that I am trying to copy the basic format of.
+
+<img title="" src="images\calculator.jpg" alt="Andriod Calculator" data-align="center">
+
+## The Process:
+
+
+
+## The Outcome:
--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -9,12 +9,92 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Container(
+                  alignment: Alignment.centerRight,
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: const Text(
+                    style: TextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    '2+2',
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: const Text(
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                    ),
+                    '4',
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                calculatorButton,
+                calculatorButton,
+                calculatorButton,
+                calculatorButton,
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                calculatorButton,
+                calculatorButton,
+                calculatorButton,
+                calculatorButton,
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                calculatorButton,
+                calculatorButton,
+                calculatorButton,
+                calculatorButton,
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                calculatorButton,
+                calculatorButton,
+                calculatorButton,
+                calculatorButton,
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                calculatorButton,
+                calculatorButton,
+                calculatorButton,
+                calculatorButton,
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+// TODO: Turn this into a function to take different text/icons
+var calculatorButton = ElevatedButton(
+  onPressed: () {},
+  child: const Text('X'),
+);
